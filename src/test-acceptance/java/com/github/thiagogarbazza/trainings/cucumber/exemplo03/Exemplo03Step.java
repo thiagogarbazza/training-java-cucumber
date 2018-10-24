d2e01a1b086342757113e9eff536a64525b109a6
Thiago Garbazza <thiagogarbazza@gmail.com>
2018-10-24 08:07:47 -0300

    criando exeplo 03

---
+++ src/test-acceptance/java/com/github/thiagogarbazza/trainings/cucumber/exemplo03/Exemplo03Step.java
@@ -0,0 +1,51 @@
+package com.github.thiagogarbazza.trainings.cucumber.exemplo03;
+
+import com.github.thiagogarbazza.trainings.cucumber.CalendarioService;
+import cucumber.api.java.Before;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.E;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+import java.time.LocalDate;
+
+import static com.github.thiagogarbazza.trainings.cucumber.CalendarioService.getCalendarioService;
+import static com.github.thiagogarbazza.trainings.cucumber.LocalDateUtil.toLocalDate;
+import static org.junit.Assert.assertEquals;
+
+public class Exemplo03Step {
+
+  private CalendarioService calendarioService = getCalendarioService();
+  private LocalDate data1;
+  private LocalDate data2;
+  private Integer quantidadeDiasUteis;
+
+  @Before
+  public void before() {
+    data1 = null;
+    data2 = null;
+    quantidadeDiasUteis = null;
+    CalendarioService.FERIADOS.clear();
+  }
+
+  @E("^considerando que o dia \"([^\"]*)\" é feriado\\.$")
+  public void considerandoQueODiaÉFeriado(String arg0) {
+    CalendarioService.FERIADOS.add(toLocalDate(arg0));
+  }
+
+  @Dado("^entre as datas \"([^\"]*)\" e \"([^\"]*)\"\\.$")
+  public void entreAsDatasE(String arg0, String arg1) {
+    this.data1 = toLocalDate(arg0);
+    this.data2 = toLocalDate(arg1);
+  }
+
+  @Então("^o sistema responde que são \"([^\"]*)\" dias úteis\\.$")
+  public void oSistemaRespondeQueSãoDiasÚteis(String arg0) {
+    assertEquals(Integer.valueOf(arg0), quantidadeDiasUteis);
+  }
+
+  @Quando("^perguntamos quantos dias úteis existem\\.$")
+  public void perguntamosQuantosDiasÚteisExistem() {
+    this.quantidadeDiasUteis = calendarioService.contarDiasUteis(data1, data2);
+  }
+}
---
+++ src/test-acceptance/resources/com/github/thiagogarbazza/trainings/cucumber/exemplo03/Exemplo03AT.feature
@@ -0,0 +1,36 @@
+# language: pt
+
+Funcionalidade: Contar a qualtidade de dias úteis entre duas datas.
+
+  Cenário: Contar a quantidade de dias úteis.
+    Dado entre as datas "01/10/2018" e "14/10/2018".
+    E considerando que o dia "12/10/2018" é feriado.
+    Quando perguntamos quantos dias úteis existem.
+    Então o sistema responde que são "9" dias úteis.
+
+  Cenário: Contar a quantidade de dias úteis.
+    Dado entre as datas "01/11/2018" e "16/11/2018".
+    E considerando que o dia "02/11/2018" é feriado.
+    E considerando que o dia "15/11/2018" é feriado.
+    Quando perguntamos quantos dias úteis existem.
+    Então o sistema responde que são "10" dias úteis.
+
+  Cenário: Contar a quantidade de dias úteis.
+    Dado entre as datas "01/12/2018" e "16/12/2018".
+    E considerando que o dia "08/12/2018" é feriado.
+    Quando perguntamos quantos dias úteis existem.
+    Então o sistema responde que são "10" dias úteis.
+
+  Esquema do Cenário: Contar a quantidade de dias úteis.
+    Dado entre as datas "<data 1>" e "<data 2>".
+    E considerando que o dia "12/10/2018" é feriado.
+    E considerando que o dia "02/11/2018" é feriado.
+    E considerando que o dia "15/11/2018" é feriado.
+    E considerando que o dia "08/12/2018" é feriado.
+    Quando perguntamos quantos dias úteis existem.
+    Então o sistema responde que são "<resultado>" dias úteis.
+    Exemplos:
+      | data 1     | data 2     | resultado |
+      | 01/10/2018 | 14/10/2018 | 9         |
+      | 01/11/2018 | 16/11/2018 | 10        |
+      | 01/12/2018 | 16/12/2018 | 10        |
