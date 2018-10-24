0e41c6687546369416e446912f72da56f9cf7e51
Thiago Garbazza <thiagogarbazza@gmail.com>
2018-10-24 08:20:44 -0300

    criando exemplo 4

---
+++ src/test-acceptance/java/com/github/thiagogarbazza/trainings/cucumber/exemplo04/Exemplo04AT.java
@@ -0,0 +1,10 @@
+package com.github.thiagogarbazza.trainings.cucumber.exemplo04;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:com/github/thiagogarbazza/trainings/cucumber/exemplo04/Exemplo04AT.feature")
+public class Exemplo04AT {
+}
